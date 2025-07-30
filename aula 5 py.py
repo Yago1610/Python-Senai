95c6db70c39995c531fdc763b1adfe171d92e66c
Yago1610 <yagostecconi@gmail.com>
2025-07-29 21:11:42 -0300

    Add files via upload

---
+++ aula 5 py.py
@@ -0,0 +1,40 @@
+resultado =  ( 4 * 6 )/2
+print('resultado:', resultado)
+
+resultado = ( 5 + 7 )
+print ('resultado:', resultado)
+
+resultado = ( 3 ** 4 )
+print ('resultado:', resultado)
+
+media  =  ( 8 + 12 + 15 ) / 3
+print('Resultado: ', media )
+
+resultado = (10 - 2)
+print ('resultado:', resultado )
+resultado = (resultado * 5)
+print ('resultado:', resultado)
+
+resultado = (27 / 3)
+print ('resultado:', resultado )
+resultado = (resultado + 5)
+print ('resultado:', resultado)
+
+
+
+
+
+
+resultado = (3 * 8)
+print ('resultado:', resultado )
+resultado = (resultado - 15)
+print ('resultado:', resultado)
+resultado = (resultado / 2)
+print ('resultado:', resultado)
+
+
+
+
+
+resultado = ( 2 ** 10 )
+print ('resultado:', resultado)
